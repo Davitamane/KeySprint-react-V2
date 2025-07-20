ba5ae6840a2e4c62be38333e37e5731023dd86f3
Davitamane <datokhizanishvilii005@gmail.com>
2025-07-20 18:14:15 +0400

    pls dont break again

--- src/components/NormalMode/NormalMode.jsx
+++ src/components/NormalMode/NormalMode.jsx
@@ -1,7 +1,13 @@
-// import styles from './NormalMode.module.css'
+import styles from "./NormalMode.module.css";
+import GameSettings from "./components/GameSettings";
 
 function NormalMode() {
-  return <div>normal</div>;
+  return (
+    <div className={styles.game_container}>
+      <GameSettings />
+      <div className={styles.text_container}></div>
+    </div>
+  );
 }
 
 export default NormalMode;
---
+++ src/components/NormalMode/components/GameSettings.jsx
@@ -0,0 +1,14 @@
+import styles from "./GameSettings.module.css";
+
+function GameSettings() {
+  return (
+    <div>
+      <button>Punctuation</button>
+      <button>10 words</button>
+      <button>25 words</button>
+      <button>50 words</button>
+    </div>
+  );
+}
+
+export default GameSettings;
